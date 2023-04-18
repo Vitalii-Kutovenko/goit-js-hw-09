e98bed041dd2d5d378ff7829d11ec601419624c4
Vitalii Kutovenko <idorikk@gmail.com>
2023-04-18 21:40:22 +0300

    2

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,24 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  
+  let intervalId = null;
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+  const body = document.body;
+  
+  function startChangingColor() {
+    startButton.disabled = true;
+    intervalId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+  
+  function stopChangingColor() {
+    startButton.disabled = false;
+    clearInterval(intervalId);
+  }
+  
+  startButton.addEventListener('click', startChangingColor);
+  stopButton.addEventListener('click', stopChangingColor);
+  
